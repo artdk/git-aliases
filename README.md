d031bb5255f1903401cbf86c559413ef7803f118
Arthur Degen-Knifton <artius89@gmail.com>
2019-12-24 23:50:16 +0100

    Split aliases into seperate files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# git-aliases
-my loosely-associated git and jira aliases and scripts
+# ART'S GIT ALIASES
+My collection of git aliases. Suggestions highly welcome!
+
+To add this to your shell add `source {repository root directory}/git-aliases/git.alias` to your `.bashrc` or `.zshrc`
---
+++ help.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ALIAS_DIR=$( dirname $0 )
+ALIASES=('branch' 'cherrypick' 'commit' 'merge' 'pull' 'push' 'rebase' 'reset' 'stash' 'status')
+if [ -z $1 ]
+then
+echo "====== ART'S GIT ALIASES ======"
+echo "Type 'gitaliases {operation}' to see the aliases for a given operation"
+echo "    eg. 'gitaliases commit'"
+echo ''
+echo ''
+for ALIAS in ${ALIASES[@]}
+do
+cat $ALIAS_DIR/$ALIAS.alias
+echo ''
+echo ''
+done
+exit
+else
+cat $ALIAS_DIR/$1.alias
+fi
